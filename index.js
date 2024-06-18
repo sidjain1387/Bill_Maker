3af0d734a221b0a35c9de7fa3f025257a3ccd327
Siddhant <sidjain1387@gmail.com>
2024-06-18 23:13:15 +0530

    bill of hotel_booking done

--- index.js
+++ index.js
@@ -8,6 +8,9 @@ const app= express();
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
+app.use(express.json());
+
+
 
 const db=new pg.Client({
     user: "postgres",
@@ -58,6 +61,7 @@ let currentbillno=0;
 let currentbillerid=0;
 let currentbillername="";
 let choice=0;
+let date="";
 
 app.post("/addcustomer",async (req,res)=>{
     const name=req.body.customer_name;
@@ -143,7 +147,7 @@ app.post("/addhotelbooking",async (req,res)=>{
     }
 })
 
-let date="";
+
 app.post("/prebill",async(req,res)=>{
     currentbillno=req.body.bill_id;
     currentbillerid=req.body.customer_id;
@@ -155,42 +159,70 @@ app.post("/prebill",async(req,res)=>{
     currentbillername=await db.query("SELECT customer_name FROM customer_info WHERE customer_id=$1",[currentbillerid]);
     if(choice==1){
         let current_air_tickets=await db.query("SELECT * FROM air_ticket WHERE air_ticket_biller_id=$1 AND air_ticket_bill_id is NULL",[currentbillerid]);
-        res.render("prebill_air_ticket.ejs",{bill_id:currentbillno,
-            customer_name:currentbillername.rows[0].customer_name,
-            air_ticket:current_air_tickets.rows});
+        res.render("prebill_air_ticket.ejs",{bill_id:currentbillno,customer_name:currentbillername.rows[0].customer_name,air_ticket:current_air_tickets.rows});
         
     }
-    else{
-
+    else if(choice==2){
         let current_hotel_booking=await db.query("SELECT * FROM hotel_booking WHERE hotel_booking_biller_id=$1 AND hotel_booking_bill_id is NULL",[currentbillerid]);
-        res.render("prebill_hotel.ejs",{bill_id:currentbillno,
-            customer_name:currentbillername.rows[0].customer_name,
-            hotel_booking:current_hotel_booking.rows});
+        console.log("/prebill current hotel booking",current_hotel_booking.rows);
+        res.render("prebill_hotel.ejs",{bill_id:currentbillno,customer_name:currentbillername.rows[0].customer_name,hotel_booking:current_hotel_booking.rows});
     }
 })
 
 app.post("/bill_hotel",async (req,res)=>{
-    let selected_hotel_booking=[];
-    
-    for(let j=0;j<req.body.hotel_booking.length;j++){
-        let ticket=parseInt(req.body.hotel_booking[j]);
-        selected_hotel_booking.push(ticket);
-    };
-    selected_hotel_booking.forEach((ticket)=>{
-        ticket=parseInt(ticket);
-        let query_2345=db.query("UPDATE hotel_booking SET hotel_booking_biller_id=$1,hotel_booking_bill_id=$2 WHERE hotel_booking_id=$3",[currentbillerid,currentbillno,ticket]);
-    })
+    let selected_hotel_bookings = [];
+    let service_charge_quantity = req.body.service_charge_quantity;
+    let service_charge_rate = req.body.service_charge_rate;
+    let sac_code = parseInt(req.body.sac_code_original);
+
+    let service_charge = parseInt(service_charge_quantity) * parseInt(service_charge_rate);
+    let original_selected_bookings=req.body.selected_bookings;
+    console.log("original selected bookings",typeof(original_selected_bookings));
+
+    console.log("1");
+    console.log("service charge",service_charge);
+    console.log("currentbillno",currentbillno);
+    await db.query("UPDATE bill_info SET bill_service_charge = $1 WHERE bill_id = $2;",[parseInt(service_charge),parseInt(currentbillno)]);
+    console.log("2");
+
+    console.log("/bill_hotel hotel_booking",original_selected_bookings);
+
+    if(typeof(original_selected_bookings)=='string'){
+        let ticket=parseInt(original_selected_bookings);
+        selected_hotel_bookings.push(ticket);
+    }
+    else{
+        for(let j=0;j<original_selected_bookings.length;j++){
+            
+            let ticket=parseInt(original_selected_bookings[j]);
+            
+            selected_hotel_bookings.push(ticket);
+        };
+    }
+
+
+    console.log("/bill_hotel selected hotel bookings",selected_hotel_bookings);
+
+    console.log("/bill_hotel sac code",sac_code);
+
+
+    selected_hotel_bookings.forEach((ticket1)=>{
+        ticket1=parseInt(ticket1);
+        let query_2345=db.query("UPDATE hotel_booking SET hotel_booking_biller_id=$1,hotel_booking_bill_id=$2 WHERE hotel_booking_id=$3",[currentbillerid,currentbillno,ticket1]);
+    });
+
     let query1="SELECT * FROM hotel_booking WHERE hotel_booking_id IN";
     let where_value1="";
-    for(let i=0;i<=selected_hotel_booking.length+1;i++){
+
+    for(let i=0;i<=selected_hotel_bookings.length+1;i++){
         if(i==0){
             where_value1+="(";
         }
-        else if(i==selected_hotel_booking.length+1){
+        else if(i==selected_hotel_bookings.length+1){
             where_value1+=");";
         }
         else{
-            if(i==selected_hotel_booking.length){
+            if(i==selected_hotel_bookings.length){
                 where_value1+="$"+i;
             }
             else{
@@ -198,14 +230,25 @@ app.post("/bill_hotel",async (req,res)=>{
             }
         }      
     }
+    console.log("/bill_hotel where value",where_value1);
     query1+=where_value1;
-    let final_hotel_booking= await db.query(`${query1}`,selected_hotel_booking);
-    
+    console.log("/bill_hotel query1",query1);
+
+    let final_hotel_booking= await db.query(`${query1}`,selected_hotel_bookings);
+    console.log("/bill_hotel final hotel booking",final_hotel_booking.rows);
 
 
     let current_customer=await db.query("SELECT * FROM customer_info WHERE customer_id=$1",[currentbillerid]);
-    res.render("bill_hotel.ejs",{hotel_booking:final_hotel_booking.rows,
-        customer:current_customer.rows[0],sno:1,billno:currentbillno});
+    console.log("/bill_hotel current customer",current_customer.rows[0]);
+    res.render("bill_hotel.ejs",{final_final_hotel_booking:final_hotel_booking.rows,
+        customer:current_customer.rows[0],
+        sno:1,
+        billno:currentbillno,
+        date:date,
+        final_sac_code:sac_code,
+        final_service_charge:service_charge,
+        final_service_charge_quantity:service_charge_quantity,
+        final_service_charge_rate:service_charge_rate});
 });
     
 app.post("/bill_air",async(req,res)=>{
@@ -214,15 +257,24 @@ app.post("/bill_air",async(req,res)=>{
     let service_charge_quantity=req.body.service_charge_quantity;
     let service_charge_rate=req.body.bill_service_charge;
     let service_charge=parseInt(service_charge_quantity)*parseInt(service_charge_rate);
+    let original_selected_bookings=req.body.air_tickets;
     console.log("service charge",service_charge);
 
+    console.log("original selected bookings",original_selected_bookings);
     let result3 = await db.query("Insert into bill_info(bill_service_charge) values($1)",[service_charge]);
-
-    
-    for(let j=0;j<req.body.air_tickets.length;j++){
-        let ticket=parseInt(req.body.air_tickets[j]);
+    if(typeof(original_selected_bookings)=='string'){
+        let ticket=parseInt(original_selected_bookings);
         selected_air_ticket.push(ticket);
-    };
+    }
+    else{
+        for(let j=0;j<original_selected_bookings.length;j++){
+            
+            let ticket=parseInt(original_selected_bookings[j]);
+            
+            selected_air_ticket.push(ticket);
+        };
+    }
+    
     selected_air_ticket.forEach((ticket)=>{
         ticket=parseInt(ticket);
         let query_1234=db.query("UPDATE air_ticket SET air_ticket_biller_id=$1,air_ticket_bill_id=$2 WHERE air_ticket_id=$3",[currentbillerid,currentbillno,ticket]);
@@ -251,7 +303,13 @@ app.post("/bill_air",async(req,res)=>{
 
     let final_air_ticket= await db.query(`${query2}`,selected_air_ticket);
     res.render("bill_air_ticket.ejs",{air_ticket:final_air_ticket.rows,
-            customer:current_customer.rows[0],sno:1,billno:currentbillno,date:date,service_charge:service_charge,service_charge_quantity:service_charge_quantity,service_charge_rate:service_charge_rate});
+            customer:current_customer.rows[0],
+            sno:1,
+            billno:currentbillno,
+            date:date,
+            service_charge:service_charge,
+            service_charge_quantity:service_charge_quantity,
+            service_charge_rate:service_charge_rate});
 })
 
 app.listen(3000,()=>{
