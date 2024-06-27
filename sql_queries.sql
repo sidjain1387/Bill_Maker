0ec74f53889c04537cac75d9c4752f72220431c3
Siddhant <sidjain1387@gmail.com>
2024-06-27 11:25:55 +0530

    all sql queries

--- sql_queries.sql
+++ sql_queries.sql
@@ -1,3 +1,5 @@
+database name = bill;
+
 create table package_info(
 package_id serial Primary key,
 package_desc_1 text,
@@ -11,3 +13,55 @@ package_bill_id int,
 package_sac_code int,
 package_quantity int,
 package_rate int);
+
+create table hotel_booking(
+    hotel_booking_id serial Primary key,
+    hotel_name text,
+    hotel_location text,
+    hotel_nights int,
+    hotel_check_in_date text,
+    hotel_check_out_date text,
+    hotel_booking_biller_id int,
+    hotel_booking_bill_id int,
+    hotel_booking_customer_name text,
+    hotel_quantity int,
+    hotel_rate int,
+    hotel_sac_code text
+);
+
+
+create table air_ticket(
+    air_ticket_id serial Primary key,
+    flight_number text,
+    flight_date text,
+    flight_time text,
+    flight_pnr text,
+    flight_travel_location text,
+    air_ticket_biller_id int,
+    air_ticket_bill_id int,
+    air_ticket_customer_name text,
+    flight_quantity int,
+    flight_rate int,
+    flight_sac_code text
+);
+
+
+create table bill_info(
+    bill_id serial,
+    bill_customer_id int,
+    bill_date date,
+    bill_service_charge int,
+    bill_sac_code text,
+    bill_booking_type text,
+    bill_service_charge_rate text,
+    bill_service_charge_quantity text
+)
+
+create table customer_info(
+    customer_id serial Primary key,
+    customer_name text,
+    customer_address text,
+    customer_state text,
+    customer_state_code int,
+    customer_gstin text
+);
